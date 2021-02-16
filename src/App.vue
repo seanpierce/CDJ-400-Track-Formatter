9047048806887734b041b4b29309a01a04c0dd82
Sean Pierce <sumler.sean@gmail.com>
2021-02-15 20:43:01 -0800

    Add router and vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <h1>Track Formatter</h1>
-    <SelectSource />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import SelectSource from './components/SelectSource' 
-
-export default {
-  name: 'App',
-  components: {
-    SelectSource
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
