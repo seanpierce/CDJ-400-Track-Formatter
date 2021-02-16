e02b5f0435a87b9385dbc6020fe76ad71a7d5c36
Sean Pierce <sumler.sean@gmail.com>
2021-02-15 20:41:03 -0800

    Initialize electron processes

---
+++ src/components/PathSelector.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="path-selector">
+        <button @click="getPath()">{{ label }}</button>
+    </div>
+</template>
+
+<script>
+const { dialog } = require('electron').remote
+
+export default {
+
+    props: {
+        label: {
+            type: String,
+            default: 'Select Path'
+        }
+    },
+    
+    methods: {
+
+        getPath() {
+            dialog.showOpenDialog({
+                properties: ['openDirectory']
+            }).then(data => {
+                console.log(data.filePaths[0])
+            })
+        }
+    }
+}
+</script>
